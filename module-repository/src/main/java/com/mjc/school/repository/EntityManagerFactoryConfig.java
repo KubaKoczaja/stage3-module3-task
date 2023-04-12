8b279c22857f284a36cf74240904e8e48d05c3c4
Jakub Koczaja <kuba.koczaja@gmail.com>
2023-04-12 12:20:04 +0200

    @Primary added to EntityManagerFactory bean

--- module-repository/src/main/java/com/mjc/school/repository/EntityManagerFactoryConfig.java
+++ module-repository/src/main/java/com/mjc/school/repository/EntityManagerFactoryConfig.java
@@ -3,6 +3,7 @@ package com.mjc.school.repository;
 import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.Persistence;
@@ -12,6 +13,7 @@ import javax.persistence.Persistence;
 @NoArgsConstructor
 public class EntityManagerFactoryConfig {
 		@Bean
+		@Primary
 		public static javax.persistence.EntityManagerFactory entityManagerFactory() {
 				return Persistence.createEntityManagerFactory("KAPAPLAN");
 		}
